73cd80bd81126960b514daf9e5b4b34f6fccab00
Lily <lily@lilygo.cc>
2017-12-21 17:17:22 +0800

    first commit

---
+++ TEST_button/lib/T10_button/README.md
@@ -0,0 +1,67 @@
+# M5Stack Library
+
+## Usage
+### Installing the USB Driver
+- [Download the SiLabs CP2104 Driver](https://www.silabs.com/community/interface/knowledge-base.entry.html/2017/01/10/legacy_os_softwarea-bgvU)
+(Note:If you cannot upload, please try the legacy v4 driver below. Note you will need to uninstall the v5 driver.And try to use the low band: 115200)
+
+### Installing the ESP32 Arduino Core
+- Using Arduino IDE
+  + [Instructions for Windows](docs/arduino-ide/windows.md)
+  + [Instructions for Mac](docs/arduino-ide/mac.md)
+  + [Instructions for Debian/Ubuntu Linux](docs/arduino-ide/debian_ubuntu.md)
+  + [Instructions for Fedora](docs/arduino-ide/fedora.md)
+  + [Instructions for openSUSE](docs/arduino-ide/opensuse.md)
+- [Using PlatformIO](docs/platformio.md)
+- [Building with make](docs/make.md)
+- [Using as ESP-IDF component](docs/esp-idf_component.md)
+
+### Download Library
+
+#### Using the Arduino IDE Library Manager
+
+1. Choose ```Sketch``` -> ```Include Library``` -> ```Manage Libraries...```
+2. Type ```m5stack``` into the search box.
+3. Click the row to select the library.
+4. Click the ```Install``` button to install the library.
+5. Click "File-> Examples". Here are some test programs in "M5Stack->"
+
+#### Using Git
+```sh
+cd ~/Documents/Arduino/libraries/
+git clone https://github.com/m5stack/M5Stack.git
+```
+
+## API
+See [API](https://github.com/m5stack/M5Stack/blob/master/src/M5Stack.h#L19).
+
+## Examples
+See [examples](examples) folder.
+
+
+## Hardware
+[Schematic](https://github.com/m5stack/M5-hardware/blob/master/M5_Core_SCH(20171206).pdf).
+### Pinout
+Peripheral Devices | ESP32 
+---|---
+ILI9341 RST | GPIO33 
+ILI9341 DC | GPIO27 
+ILI9341 CS | GPIO14
+ILI9341 MOSI | GPIO23
+ILI9341 CLK | GPIO18
+ILI9341 LIGHT | GPIO32
+TFCARD MOSI | GPIO23
+TFCARD MISO | GPIO19
+TFCARD CLK | GPIO18
+TFCARD CS | GPIO4
+BUTTON A | GPIO39
+BUTTON B | GPIO38
+BUTTON C | GPIO37
+SPEAKER | GPIO25
+MPU9250 SDA | GPIO21
+MPU9250 SCL | GPIO22
+GOVER SDA | GPIO21
+GOVER SCL | GPIO22
+
+### M-BUS
+![image](docs/M-BUS.jpg = 100x100)
---
+++ TEST_speaker/lib/src/M5Stack.cpp
@@ -0,0 +1,107 @@
+// Copyright (c) M5Stack. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "M5Stack.h"
+
+void M5Stack::begin() {
+    
+    // UART 
+    Serial.begin(115200);
+    Serial.flush();
+    Serial.print("M5Stack initializing...");
+
+    // I2C
+    pinMode(SCL, OUTPUT);
+    digitalWrite(SDA, 1);
+
+    // TONE
+    Speaker.begin();
+
+    // Setup the button with an internal pull-up
+    pinMode(BUTTON_A_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_B_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_C_PIN, INPUT_PULLUP);
+
+    // TF Card
+    SD.begin(TFCARD_CS_PIN);
+
+    // M5 LCD INIT
+    Lcd.begin();
+    Lcd.fillScreen(BLACK);
+    Lcd.setCursor(0, 0);
+    Lcd.setTextColor(WHITE);
+    Lcd.setTextSize(1);
+    Lcd.setBrightness(50);
+
+    // Set wakeup button
+    setWakeupButton(BUTTON_A_PIN);
+
+    Serial.println("OK.");
+}
+
+void M5Stack::update() {
+
+    //Button update
+    BtnA.read();
+    BtnB.read();
+    BtnC.read();
+
+    //Speaker update
+    Speaker.update();
+}
+
+void M5Stack::startupLogo() {
+    static uint8_t brightness, pre_brightness;
+    uint32_t length = strlen((char*)m5stack_startup_music);
+    Lcd.setBrightness(0);
+    Lcd.drawBitmap(0, 0, 320, 240, (uint16_t *)gImage_logoM5);
+    for(int i=0; i<length; i++) {
+        dacWrite(SPEAKER_PIN, m5stack_startup_music[i]>>2);
+        delayMicroseconds(40);
+        brightness = (i/157);
+        if(pre_brightness != brightness) {
+            pre_brightness = brightness;
+            Lcd.setBrightness(brightness);
+        }
+    }
+
+    for(int i=255; i>=0; i--) {
+        lcd.setBrightness(i);
+        if(i<=32) {
+            dacWrite(SPEAKER_PIN, i);
+        }
+        delay(2);
+    }
+
+    Lcd.fillScreen(BLACK);
+    delay(800);
+    for(int i=0; i>=100; i++) {
+        Lcd.setBrightness(i);
+        delay(2);
+    }
+}
+
+void M5Stack::setWakeupButton(uint8_t button) {
+    _wakeupPin = button;
+}
+
+void M5Stack::powerOFF() {
+    
+    // power off the Lcd
+    Lcd.setBrightness(0);
+    Lcd.sleep();
+
+    // ESP32 into deep sleep
+    uint64_t _wakeupPin_mask = 1ULL << _wakeupPin; 
+    USE_SERIAL.printf("Enabling EXT1 wakeup on pins GPIO%d\n", _wakeupPin);
+    esp_deep_sleep_enable_ext1_wakeup(_wakeupPin_mask , ESP_EXT1_WAKEUP_ALL_LOW);
+    
+    while(digitalRead(_wakeupPin) == LOW) {
+        delay(10);
+    }
+    USE_SERIAL.println("On deep sleep mode.");
+    esp_deep_sleep_start();
+    USE_SERIAL.println("On power OFF fail!");
+}
+
+M5Stack m5stack;
