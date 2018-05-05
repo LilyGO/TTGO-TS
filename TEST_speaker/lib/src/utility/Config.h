5ae5aeb69647e021b5c91fa24b641688f136e8b2
LilyGO <31160602+LilyGO@users.noreply.github.com>
2018-05-05 09:40:02 +0800

    Update Config.h

--- TEST_speaker/lib/src/utility/Config.h
+++ TEST_speaker/lib/src/utility/Config.h
@@ -23,8 +23,8 @@
 #define BUTTON_B 1
 #define BUTTON_C 2
 #define BUTTON_A_PIN 39
-#define BUTTON_B_PIN 35
-#define BUTTON_C_PIN 34
+#define BUTTON_B_PIN 38
+#define BUTTON_C_PIN 37
 
 // BEEP PIN
 #define SPEAKER_PIN 25
